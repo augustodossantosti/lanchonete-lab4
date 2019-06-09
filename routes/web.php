30d7d27dc78cb2678c3db4b9147892cd60481a40
Ariene Maiara <ariene.maiara@hotmail.com>
2019-06-08 23:05:16 -0300

    Exibição dos dados do banco

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/ingredientes', function () {
-    return view('ingredientes');
-});
+Route::resource('ingredientes', 'IngredienteController');
+
+// Route::get('/ingredientes', function () {
+//     return view('ingredientes');
+// });
